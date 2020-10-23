1754d69fceb5c7c41d42da971c8f31c6800154ef
胡子豪 <wuqieor@outlook.com>
2020-10-23 21:04:33 +0800

    图书模块的web层：图书添加功能及表单重复提交bug

--- src/com/huzihao/web/BookServlet.java
+++ src/com/huzihao/web/BookServlet.java
@@ -1,7 +1,9 @@
 package com.huzihao.web;
 
+import com.huzihao.pojo.Book;
 import com.huzihao.service.BookService;
 import com.huzihao.service.impl.BookServiceImpl;
+import com.huzihao.utils.WebUtils;
 
 import java.io.IOException;
 
@@ -18,7 +20,17 @@ public class BookServlet extends BaseServlet {
 
     protected void add(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
+        /*
+        1.获取请求参数，封装成Book对象
+        2.调用service.addBook
+        3.重定向到/manager/book?action=list
+         */
+        var book = WebUtils.copyParameterToBean(req.getParameterMap(), new Book());
+        service.addBook(book);
+        // 表单重复提交：
+        // 浏览器会记录最后一次请求的信息，当刷新页面时会再次发送最后一次请求
+        // 若使用重定向，则改变了最后一次请求
+        resp.sendRedirect(req.getContextPath() + "/manager/book?action=list");
     }
 
     protected void delete(HttpServletRequest req, HttpServletResponse resp)
